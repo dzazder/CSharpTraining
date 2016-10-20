45ab91ca8e4d7464d6d3da0ab5fe13e4f694ab9d
Bartosz Lampart <dzazder@gmail.com>
2016-10-20 21:46:27 +0200

    first commit

---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
